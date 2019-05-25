cb959ad890f1aa64ec8bc93d882956852203dbbc
Junaien <linennanhai@gmail.com>
2019-05-25 18:05:55 -0400

    finish 61x67 optimization

--- computer_system/cmu_13215/lab4_cache/cachelab-handout/trans.c
+++ computer_system/cmu_13215/lab4_cache/cachelab-handout/trans.c
@@ -12,8 +12,26 @@
 #include <assert.h>
 #define MIN(a,b) (a <= b? a:b)
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
-
-
+void transpose_MN_MN(int M, int N, int src[M][N], int s_i, int s_j,
+                                   int t[M][N],   int t_i, int t_j, int blk_size);
+void trans(int M, int N, int A[N][M], int B[M][N], int blk_size);
+void transpose_NM_MN(int M, int N, int src[N][M], int s_i, int s_j,
+                             int t[M][N],   int t_i, int t_j, int blk_size);
+void trans_blk(int M, int N, int A[N][M], int B[M][N]);
+/*
+ * transpose_submit - This is the solution transpose function that you
+ *     will be graded on for Part B of the assignment. Do not change
+ *     the description string "Transpose submission", as the driver
+ *     searches for that string to identify the transpose function to
+ *     be graded.
+ */
+char transpose_submit_desc[] = "Transpose submission";
+void transpose_submit(int M, int N, int A[N][M], int B[M][N])
+{
+  if(M == 32)trans(M,N,A,B,8);
+  else if(M == 64) trans(M,N,A,B,8);
+  else trans_blk(M,N,A,B);
+}
 
 /*
  * You can define additional transpose functions below. We've defined
@@ -37,12 +55,8 @@ void trans(int M, int N, int A[N][M], int B[M][N], int blk_size)
           }
         }
         //traspose back
-        for(int k = 0; k < blk_size; k++){
-          for(int z = 0; z < blk_size; z++){
-            B[k + i][z + j] = B[i + z][(j + blk_size) % effective_n + k];
-          }
-        }
-        //assert traspose back
+        transpose_MN_MN(M, N, B, i, (j + blk_size) % effective_n, B, i, j, blk_size);
+        // assert traspose back
         // for(int k = 0; k < blk_size; k++)
         //   for(int z = 0; z < blk_size; z++){
         //     assert(A[i + k][z + j] == B[z+j][i+k]);
@@ -50,11 +64,7 @@ void trans(int M, int N, int A[N][M], int B[M][N], int blk_size)
         //tranpose below to right
         int belw_base_i  = (i + blk_size) % effective_n ;
         int right_base_j = (blk_size + j) % effective_n;
-        for(int k = 0; k < blk_size; k++){
-          for(int z = 0; z < blk_size; z++){
-             B[k + i][z + right_base_j] = A[z + belw_base_i][k + j];
-          }
-        }
+        transpose_NM_MN(M,N,A,belw_base_i, j, B, i, right_base_j, blk_size);
     }
     //regular transpose
     for(int i = 0; i < N; i+= blk_size)
@@ -62,29 +72,31 @@ void trans(int M, int N, int A[N][M], int B[M][N], int blk_size)
         //transposing blocks
         if((i == j || (j + blk_size) % effective_n == i) &&
            (i < effective_n && j < effective_n))continue;
-        for(int k = i; k < N && (k - i) < blk_size; k++){
-          for(int z = j; j < M && (z - j) < blk_size; z++){
-            B[z][k] = A[k][z];
+        if(i + blk_size > N || j + blk_size > M){
+          for(int k = i; k < N && (k - i) < blk_size; k++){
+            for(int z = j; j < M && (z - j) < blk_size; z++){
+              B[z][k] = A[k][z];
+            }
           }
-        }
+        }else transpose_NM_MN(M,N,A,i, j, B, j, i, blk_size);
+
       }
 }
 
-/*
- * transpose_submit - This is the solution transpose function that you
- *     will be graded on for Part B of the assignment. Do not change
- *     the description string "Transpose submission", as the driver
- *     searches for that string to identify the transpose function to
- *     be graded.
- */
-char transpose_submit_desc[] = "Transpose submission";
-void transpose_submit(int M, int N, int A[N][M], int B[M][N])
+char trans_blk_desc[] = "simple blocking";
+void trans_blk(int M, int N, int A[N][M], int B[M][N])
 {
-  if(M == 32)trans(M,N,A,B,8);
-  else if(M == 64) trans(M,N,A,B,4);
-  else trans(M,N,A,B,18);
+    int blk_size = 16;
+    for(int i = 0; i < N; i+= blk_size){
+      for(int j = 0; j < M; j+= blk_size){
+        for(int k = 0; k < blk_size; k++){
+          for(int z = 0; z < blk_size; z++){
+            if(j + z < M && i + k < N)B[j + z][i + k] = A[i + k][j + z];
+          }
+        }
+      }
+    }
 }
-
 
 
 /*
@@ -100,7 +112,7 @@ void registerFunctions()
     registerTransFunction(transpose_submit, transpose_submit_desc);
 
     /* Register any additional transpose functions */
-    // registerTransFunction(trans, trans_desc);
+    registerTransFunction(trans_blk, trans_blk_desc);
     // registerTransFunction(trans_col, trans_col_desc);
     // registerTransFunction(trans_blocking_4, trans_blocking_4_desc);
 
@@ -123,4 +135,35 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
         }
     }
     return 1;
+}
+
+//blk_size must be divisible by 2
+void transpose_MN_MN(int M, int N, int src[M][N], int s_i, int s_j,
+                             int t[M][N],   int t_i, int t_j, int blk_size){
+  for(int d = 0; d < 4; d++){
+
+    int shift_i   = 0,  shift_j = 0;
+    if(d == 0 || d == 1){shift_i = blk_size / 2;}
+    if(d == 2 || d == 1){shift_j = blk_size / 2;}
+    for(int k = 0; k < blk_size / 2; k++){
+      for(int z = 0; z < blk_size / 2; z++){
+        t[t_i + shift_j + k][t_j + shift_i + z] = src[s_i + shift_i + z][s_j + shift_j + k];
+      }
+    }
+  }
+}
+
+//blk_size must be divisible by 2
+void transpose_NM_MN(int M, int N, int src[N][M], int s_i, int s_j,
+                             int t[M][N], int t_i, int t_j, int blk_size){
+  for(int d = 0; d < 4; d++){
+    int shift_i   = 0,  shift_j = 0;
+    if(d == 1 || d == 2){shift_i = blk_size / 2;}
+    if(d == 3 || d == 2){shift_j = blk_size / 2;}
+    for(int k = 0; k < blk_size / 2; k++){
+      for(int z = 0; z < blk_size / 2; z++){
+        t[t_i + shift_j + k][t_j + shift_i + z] = src[s_i + shift_i + z][s_j + shift_j + k];
+      }
+    }
+  }
 }
