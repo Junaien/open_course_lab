9b433c487c225a12a7514c09c491f94b19246afa
ENLIN <linennanhai@gmail.com>
2019-11-03 09:10:51 -0500

    routine

--- crytography/project/kangaroo.py
+++ crytography/project/kangaroo.py
@@ -15,60 +15,49 @@ def f(x, k):
 
 # g^x = h (mod p)
 def kangaroo(g, h, p, b, k, n):
-  answer = -1
-  z = 0
-
+  
   # step 0: initialize tame kangroo, distance = b
   t = power(g, b, p)
-  d_t = b
+  d_t = 0
  
   # step 1: tame kangroo jump n times then set the trap
   for i in range(0, n):
-    d_t = (d_t + f(t, k))
-    t = (power(g, f(t, k), p) * t) % p
+    hop_distance = (2**f(t, k))
+    d_t = (d_t + hop_distance)
+    t = (power(g, hop_distance, p) * t) % p
   
-  print ("trap set")
+  print (f"trap set with right bound {d_t + b}")
+
+  answer = -1
+  z = 0
   while answer == -1:
     # step 2: initialize wild kangroo
     w = (h * power(g, z, p))
     d_w = z
 
     # step 3: wild kangroo jump
-    while d_w <= d_t:
+    while (d_t  - d_w + b) >= 1:
       if w == t:
         # we trap the wild kangroo
-        answer = (d_t  - d_w)
+        answer = (d_t  - d_w + b)
         break
       else:
-        d_w = (d_w + f(w, k))
-        w = (power(g, f(w, k), p) * w)  % p
-
+        hop_distance = (2**f(w, k))
+        d_w = (d_w + hop_distance)
+        w = (power(g, hop_distance, p) * w)  % p
+    
     z = z + 1
+    print(f"trying new z = {z}")
 
   return answer
 
 def test():
-  # p = 61845915503831114091865164962647232917206327870669899
-  # g = 3
-  # x = 13215411345132
-  # h = power(g, x, p)
-  # print(kangaroo(g, h, p, x + 1000000000, 1000, 100000))
-
-  # g = 2
-  # h = 208891284998759
-  # p = 247457076132467
-  # print(kangaroo(g, h, p, p - 1, 100, 10000000))
-
   # g^x = h (mod p)
-  g = 3
-  x = 3124532153132532131231243143123124
-  p = 61845915503831114091865164962647232917206327870669899
-  h = power(g, x, p)
+  g = 2
+  p = 247457076132467
+  h = 208891284998759
 
-  # set b very close to x
-  b = x + 900000000
-  
-  print(kangaroo(g, h, p, b, 1000, 100000))
+  print(kangaroo(g, h, p, p - 1, 25, 10000000))
 
 def main():
   test()
